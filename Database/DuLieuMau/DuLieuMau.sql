a2dc36ea9b37071d282c955c3191eaff68b200e9
Hosy3024 <134937842+Hosy3024@users.noreply.github.com>
2024-07-04 19:27:37 +0700

    Them du lieu mau cho muon tra sach

--- Database/DuLieuMau/DuLieuMau.sql
+++ Database/DuLieuMau/DuLieuMau.sql
@@ -1,6 +1,20 @@
 
 SET DATEFORMAT DMY
 
+-- Du lieu mau bang QUY_DINH
+INSERT INTO QUY_DINH VALUES (18, 55, 6, 8, 5, 7, 10, 1000)
+
+-- Số tuổi nhỏ nhất là 18 tuổi
+-- Số tuổi lớn nhất là 55 tuổi
+-- Thời hạn thẻ độc giả là 6 tháng
+-- Số năm xuất bản có thể nhập là 8 năm
+-- Số sách độc giả có thể mượn trong 7 ngày là 5 quyển
+-- Số ngày giới hạn số sách độc giả mượn là 7 ngày
+-- Thời hạn độc giả có thể mượn sách là 10 ngày
+-- Phí phạt nếu trả sách trễ là 1000đ/ngày
+
+
+
 -- Du lieu mau cho bang TAIKHOAN
 INSERT INTO TAIKHOAN (MaTK, TK_Username, TK_Password, LoaiTK) VALUES
 ('TK001', 'user1', 'password1', 'Reader'),
@@ -30,27 +44,26 @@ INSERT INTO TAIKHOAN (MaTK, TK_Username, TK_Password, LoaiTK) VALUES
 
 -- Du lieu mau cua bang THE_DOCGIA
 INSERT INTO THE_DOCGIA (MaDG, HoTen, MaTaiKhoan, NgaySinh, DiaChi, Email, Sdt, NgayLapTheDocGia, NgayHetHan) VALUES
-('DG001', 'Nguyễn Văn An', 'TK001', '01-01-1990', '123 Đường A', 'an.nguyen@example.com', '0912345678', '01-01-2023', '01-01-2024'),
-('DG002', 'Trần Thị Bích', 'TK002', '15-02-1985', '456 Đường B', 'bich.tran@example.com', '0912345679', '01-02-2023', '01-02-2024'),
-('DG003', 'Lê Hoàng Cường', 'TK003', '20-03-1979', '789 Đường C', 'cuong.le@example.com', '0912345680', '01-03-2023', '01-03-2024'),
-('DG004', 'Phạm Minh Duy', 'TK004', '10-04-1992', '101 Đường D', 'duy.pham@example.com', '0912345681', '01-04-2023', '01-04-2024'),
-('DG005', 'Hoàng Thị Em', 'TK005', '25-05-1987', '202 Đường E', 'em.hoang@example.com', '0912345682', '01-05-2023', '01-05-2024'),
-('DG006', 'Bùi Văn Phát', 'TK006', '30-06-1983', '303 Đường F', 'phat.bui@example.com', '0912345683', '01-06-2023', '01-06-2024'),
-('DG007', 'Đỗ Thị Gấm', 'TK007', '15-07-1995', '404 Đường G', 'gam.do@example.com', '0912345684', '01-07-2023', '01-07-2024'),
-('DG008', 'Vũ Hoàng Hà', 'TK008', '10-08-1981', '505 Đường H', 'ha.vu@example.com', '0912345685', '01-08-2023', '01-08-2024'),
-('DG009', 'Phạm Văn Khang', 'TK009', '05-09-1988', '606 Đường I', 'khang.pham@example.com', '0912345686', '01-09-2023', '01-09-2024'),
-('DG010', 'Nguyễn Thị Lan', 'TK010', '20-10-1993', '707 Đường J', 'lan.nguyen@example.com', '0912345687', '01-10-2023', '01-10-2024'),
-('DG011', 'Trần Văn Minh', 'TK011', '25-11-1984', '808 Đường K', 'minh.tran@example.com', '0912345688', '01-11-2023', '01-11-2024'),
-('DG012', 'Lê Thị Ngọc', 'TK012', '30-12-1990', '909 Đường L', 'ngoc.le@example.com', '0912345689', '01-12-2023', '01-12-2024'),
-('DG013', 'Hoàng Văn Nam', 'TK013', '15-01-1986', '111 Đường M', 'nam.hoang@example.com', '0912345690', '15-01-2023', '15-01-2024'),
-('DG014', 'Bùi Thị Oanh', 'TK014', '10-02-1991', '222 Đường N', 'oanh.bui@example.com', '0912345691', '15-02-2023', '15-02-2024'),
-('DG015', 'Đỗ Văn Phú', 'TK015', '05-03-1978', '333 Đường O', 'phu.do@example.com', '0912345692', '15-03-2023', '15-03-2024'),
-('DG016', 'Vũ Thị Quyên', 'TK016', '20-04-1989', '444 Đường P', 'quyen.vu@example.com', '0912345693', '15-04-2023', '15-04-2024'),
-('DG017', 'Phạm Văn Sơn', 'TK017', '25-05-1982', '555 Đường Q', 'son.pham@example.com', '0912345694', '15-05-2023', '15-05-2024'),
-('DG018', 'Nguyễn Thị Tâm', 'TK018', '30-06-1994', '666 Đường R', 'tam.nguyen@example.com', '0912345695', '15-06-2023', '15-06-2024'),
-('DG019', 'Trần Văn Tài', 'TK019', '10-07-1980', '777 Đường S', 'tai.tran@example.com', '0912345696', '15-07-2023', '15-07-2024'),
-('DG020', 'Lê Thị Uyên', 'TK020', '05-08-1996', '888 Đường T', 'uyen.le@example.com', '0912345697', '15-08-2023', '15-08-2024');
-
+('DG001', 'Nguyễn Văn An', 'TK001', '01-01-1990', '123 Đường A', 'an.nguyen@example.com', '0912345678', '01-03-2024', '01-01-2024'),
+('DG002', 'Trần Thị Bích', 'TK002', '15-02-1985', '456 Đường B', 'bich.tran@example.com', '0912345679', '01-02-2024', '01-02-2024'),
+('DG003', 'Lê Hoàng Cường', 'TK003', '20-03-1979', '789 Đường C', 'cuong.le@example.com', '0912345680', '01-03-2024', '01-03-2024'),
+('DG004', 'Phạm Minh Duy', 'TK004', '10-04-1992', '101 Đường D', 'duy.pham@example.com', '0912345681', '01-04-2024', '01-04-2024'),
+('DG005', 'Hoàng Thị Em', 'TK005', '25-05-1987', '202 Đường E', 'em.hoang@example.com', '0912345682', '01-05-2024', '01-05-2024'),
+('DG006', 'Bùi Văn Phát', 'TK006', '30-06-1983', '303 Đường F', 'phat.bui@example.com', '0912345683', '01-06-2024', '01-06-2024'),
+('DG007', 'Đỗ Thị Gấm', 'TK007', '15-07-1995', '404 Đường G', 'gam.do@example.com', '0912345684', '01-07-2024', '01-07-2024'),
+('DG008', 'Vũ Hoàng Hà', 'TK008', '10-08-1981', '505 Đường H', 'ha.vu@example.com', '0912345685', '01-08-2024', '01-08-2024'),
+('DG009', 'Phạm Văn Khang', 'TK009', '05-09-1988', '606 Đường I', 'khang.pham@example.com', '0912345686', '01-09-2024', '01-09-2024'),
+('DG010', 'Nguyễn Thị Lan', 'TK010', '20-10-1993', '707 Đường J', 'lan.nguyen@example.com', '0912345687', '01-10-2024', '01-10-2024'),
+('DG011', 'Trần Văn Minh', 'TK011', '25-11-1984', '808 Đường K', 'minh.tran@example.com', '0912345688', '01-11-2024', '01-11-2024'),
+('DG012', 'Lê Thị Ngọc', 'TK012', '30-12-1990', '909 Đường L', 'ngoc.le@example.com', '0912345689', '01-12-2024', '01-12-2024'),
+('DG013', 'Hoàng Văn Nam', 'TK013', '15-01-1986', '111 Đường M', 'nam.hoang@example.com', '0912345690', '15-03-2024', '15-01-2024'),
+('DG014', 'Bùi Thị Oanh', 'TK014', '10-02-1991', '222 Đường N', 'oanh.bui@example.com', '0912345691', '15-02-2024', '15-02-2024'),
+('DG015', 'Đỗ Văn Phú', 'TK015', '05-03-1978', '333 Đường O', 'phu.do@example.com', '0912345692', '15-03-2024', '15-03-2024'),
+('DG016', 'Vũ Thị Quyên', 'TK016', '20-04-1989', '444 Đường P', 'quyen.vu@example.com', '0912345693', '15-04-2024', '15-04-2024'),
+('DG017', 'Phạm Văn Sơn', 'TK017', '25-05-1982', '555 Đường Q', 'son.pham@example.com', '0912345694', '15-05-2024', '15-05-2024'),
+('DG018', 'Nguyễn Thị Tâm', 'TK018', '30-06-1994', '666 Đường R', 'tam.nguyen@example.com', '0912345695', '15-06-2024', '15-06-2024'),
+('DG019', 'Trần Văn Tài', 'TK019', '10-07-1980', '777 Đường S', 'tai.tran@example.com', '0912345696', '15-07-2024', '15-07-2024'),
+('DG020', 'Lê Thị Uyên', 'TK020', '05-08-1996', '888 Đường T', 'uyen.le@example.com', '0912345697', '15-08-2024', '15-08-2024');
 
 
 
@@ -58,8 +71,6 @@ INSERT INTO THE_DOCGIA (MaDG, HoTen, MaTaiKhoan, NgaySinh, DiaChi, Email, Sdt, N
 INSERT INTO THUTHU (MaTT, HoTen, MaTaiKhoan, NgayTaoTK, NgaySinh, DiaChi, Email, Sdt) VALUES
 ('TT001', 'Lê Thị Bích Ngọc', 'TK021', '01-01-2023', '05-05-1985', '123 Đường V', 'ngoc.lethi@example.com', '0912345698'),
 ('TT002', 'Nguyễn Văn Hùng', 'TK022', '01-01-2023', '20-07-1980', '456 Đường W', 'hung.nguyen@example.com', '0912345699');
-
-
 
 
 
@@ -108,14 +119,55 @@ INSERT INTO SACH (MaSach, TenSach, MaTheLoai, TacGia, NamXuatBan, NhaXuatBan, So
 ('S030', 'Hành trình về phương Đông', 'TL006', 'Baird T. Spalding', 2020, 'NXB Văn Hóa Thông Tin', 19, 340000);
 
 
--- Du lieu mau bang QUY_DINH
-INSERT INTO QUY_DINH VALUES (18, 55, 6, 8, 5, 7, 10, 1000)
 
--- Số tuổi nhỏ nhất là 18 tuổi
--- Số tuổi lớn nhất là 55 tuổi
--- Thời hạn thẻ độc giả là 6 tháng
--- Số năm xuất bản có thể nhập là 8 năm
--- Số sách độc giả có thể mượn trong 7 ngày là 5 quyển
--- Số ngày giới hạn số sách độc giả mượn là 7 ngày
--- Thời hạn độc giả có thể mượn sách là 10 ngày
--- Phí phạt nếu trả sách trễ là 1000đ/ngày
+-- Dữ liệu mẫu cho bảng PHIEU_MUON_SACH
+INSERT INTO PHIEU_MUON_SACH (MaPhieuMuon, MaDG, MaTT, SoLuongSach, NgayMuon) VALUES 
+('PM001', 'DG001', 'TT001', 6, '15-03-2024'),
+('PM002', 'DG002', 'TT002', 3, '20-02-2024'),
+('PM003', 'DG003', 'TT001', 1, '10-03-2024'),
+('PM004', 'DG004', 'TT002', 14, '05-04-2024'),
+('PM005', 'DG005', 'TT001', 4, '25-05-2024'),
+('PM006', 'DG006', 'TT002', 3, '12-07-2024'),
+('PM007', 'DG007', 'TT002', 1, '05-08-2024'),
+('PM008', 'DG008', 'TT001', 4, '18-09-2024'),
+('PM009', 'DG009', 'TT001', 5, '22-10-2024'),
+('PM010', 'DG010', 'TT001', 2, '30-11-2024');
+
+-- Dữ liệu mẫu cho bảng PHIEU_TRA_SACH
+INSERT INTO PHIEU_TRA_SACH (MaPhieuTra, MaPhieuMuon, MaDG, MaTT, NgayTra, SoLuongSach, SoNgayTre, SoTienPhat) VALUES 
+('PT001', 'PM001', 'DG001', 'TT001', '25-03-2024', 3, 0, 0),
+('PT002', 'PM002', 'DG002', 'TT002', '01-03-2024', 2, 5, 50000),
+('PT003', 'PM003', 'DG003', 'TT001', '20-03-2024', 1, 0, 0),
+('PT004', 'PM004', 'DG004', 'TT002', '10-05-2024', 4, 10, 100000),
+('PT005', 'PM005', 'DG005', 'TT001', '05-06-2024', 2, 2, 20000),
+('PT006', 'PM006', 'DG006', 'TT002', '20-07-2024', 2, 0, 0),
+('PT007', 'PM007', 'DG007', 'TT002', '15-08-2024', 1, 0, 0),
+('PT008', 'PM008', 'DG008', 'TT001', '01-10-2024', 3, 0, 0),
+('PT009', 'PM009', 'DG009', 'TT001', '05-11-2024', 4, 0, 0),
+('PT010', 'PM010', 'DG010', 'TT001', '10-12-2024', 2, 0, 0);
+
+-- Dữ liệu mẫu cho bảng CHITIET_MUONTRA
+INSERT INTO CHITIET_MUONTRA (MaPhieuMuon, MaSach, SoLuong) VALUES 
+('PM001', 'S001', 1),
+('PM001', 'S002', 2),
+('PM001', 'S003', 3),
+('PM002', 'S004', 2),
+('PM002', 'S005', 1),
+('PM003', 'S006', 1),
+('PM004', 'S007', 4),
+('PM004', 'S008', 2),
+('PM004', 'S009', 3),
+('PM004', 'S010', 5),
+('PM005', 'S011', 2),
+('PM005', 'S012', 2),
+('PM006', 'S013', 2),
+('PM006', 'S014', 1),
+('PM007', 'S015', 1),
+('PM008', 'S016', 2),
+('PM008', 'S017', 1),
+('PM008', 'S018', 1),
+('PM009', 'S019', 2),
+('PM009', 'S020', 2),
+('PM009', 'S021', 1),
+('PM010', 'S022', 1),
+('PM010', 'S023', 1);
