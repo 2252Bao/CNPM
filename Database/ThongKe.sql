ffa9d7cb8f1604bffdd3e5f70a7285924a06bcb9
Hosy3024 <134937842+Hosy3024@users.noreply.github.com>
2024-07-06 01:30:49 +0700

    Co the thong ke theo thang bat ky, nam bat ky

--- Database/ThongKe.sql
+++ Database/ThongKe.sql
@@ -1,14 +1,33 @@
-﻿
+
 -- Độc giả mượn nhiều sách nhất
-SELECT TOP 15 dg.MaDG, dg.HoTen, dg.Sdt, dg.Email, dg.DiaChi,SUM(ct.SoLuong) AS TongSoLuongSachMuon
-FROM THE_DOCGIA dg JOIN PHIEU_MUON_SACH pm ON dg.MaDG = pm.MaDG JOIN CHITIET_MUONTRA ct ON pm.MaPhieuMuon = ct.MaPhieuMuon
-GROUP BY dg.MaDG, dg.HoTen, dg.Sdt, dg.Email, dg.DiaChi
-ORDER BY TongSoLuongSachMuon DESC, dg.MaDG ASC
+DECLARE @Year INT;
+DECLARE @Month INT;
+
+SELECT @Year = tg.Nam,		-- Có thể thay đổi thời gian thống kê bằng cách cập nhật giá trị trong bảng ThoiGian_ThongKe
+	   @Month = tg.Thang
+FROM ThoiGian_ThongKe tg
+
+SELECT TOP 15 DG.MaDG, DG.HoTen, DG.Sdt, DG.Email, DG.DiaChi,SUM(CTMT.SoLuong) AS TongSoLuongSachMuon
+FROM THE_DOCGIA DG JOIN PHIEU_MUON_SACH PMS ON DG.MaDG = PMS.MaDG JOIN CHITIET_MUONTRA CTMT ON PMS.MaPhieuMuon = CTMT.MaPhieuMuon
+WHERE YEAR(PMS.NgayMuon) = @Year AND MONTH(PMS.NgayMuon) = @Month
+GROUP BY DG.MaDG, DG.HoTen, DG.Sdt, DG.Email, DG.DiaChi
+ORDER BY TongSoLuongSachMuon DESC, DG.MaDG ASC;
+
 
 
 -- Thống kê sách được mượn nhiều nhất
 
-SELECT TOP 15 s.MaSach, s.TenSach, s.TacGia, s.NhaXuatBan, s.NamXuatBan, SUM(ct.SoLuong) AS TongSoLuongDuocMuon
-FROM SACH s JOIN CHITIET_MUONTRA ct ON s.MaSach = ct.MaSach
-GROUP BY s.MaSach, s.TenSach, s.TacGia, s.NhaXuatBan, s.NamXuatBan
-ORDER BY TongSoLuongDuocMuon DESC, s.MaSach ASC
+DECLARE @Year INT;
+DECLARE @Month INT;
+
+SELECT @Year = tg.Nam,		-- Có thể thay đổi thời gian thống kê bằng cách cập nhật giá trị trong bảng ThoiGian_ThongKe
+	   @Month = tg.Thang
+FROM ThoiGian_ThongKe tg
+
+SELECT TOP 15 S.MaSach, S.TenSach, S.TacGia, S.NhaXuatBan, S.NamXuatBan,SUM(CTMT.SoLuong) AS TongSoLuongSachMuon
+FROM SACH S
+JOIN CHITIET_MUONTRA CTMT ON S.MaSach = CTMT.MaSach
+JOIN PHIEU_MUON_SACH PMS ON CTMT.MaPhieuMuon = PMS.MaPhieuMuon
+WHERE YEAR(PMS.NgayMuon) = @Year AND MONTH(PMS.NgayMuon) = @Month
+GROUP BY S.MaSach, S.TenSach, S.TacGia, S.NhaXuatBan, S.NamXuatBan
+ORDER BY TongSoLuongSachMuon DESC, S.MaSach ASC
