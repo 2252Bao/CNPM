a5686831e9363f41a8d609271c52afdf8f532ab7
Hosy3024 <134937842+Hosy3024@users.noreply.github.com>
2024-07-07 14:45:55 +0700

    Update Library.sql

--- Database/Library.sql
+++ Database/Library.sql
@@ -1,112 +1,118 @@
-CREATE DATABASE QUANLYTHUVIEN
-GO
-USE QUANLYTHUVIEN
-GO
+	CREATE DATABASE QUANLYTHUVIEN
+	GO
+	USE QUANLYTHUVIEN
+	GO
 
+	SET DATEFORMAT DMY
 	
 
-CREATE TABLE TAIKHOAN(
-	MaTK VARCHAR(255) PRIMARY KEY,
-	TK_Username VARCHAR(255),
-	TK_Password VARCHAR(255),
-	LoaiTK VARCHAR(255)
-)
+	CREATE TABLE TAIKHOAN(
+		MaTK VARCHAR(255) PRIMARY KEY,
+		TK_Username VARCHAR(255),
+		TK_Password VARCHAR(255),
+		LoaiTK VARCHAR(255)
+	)
 
-CREATE TABLE THE_DOCGIA(
-	MaDG VARCHAR(255) PRIMARY KEY,
-	HoTen VARCHAR(255),
-	MaTaiKhoan VARCHAR(255) REFERENCES TAIKHOAN(MaTK),
-	NgaySinh SMALLDATETIME,
-	DiaChi VARCHAR(255),
-	Email VARCHAR(255),
-	Sdt VARCHAR(255),
-	NgayLapTheDocGia SMALLDATETIME,
-	NgayHetHan SMALLDATETIME
-)
+	CREATE TABLE THE_DOCGIA(
+		MaDG VARCHAR(255) PRIMARY KEY,
+		HoTen VARCHAR(255),
+		MaTaiKhoan VARCHAR(255) REFERENCES TAIKHOAN(MaTK),
+		NgaySinh SMALLDATETIME,
+		DiaChi VARCHAR(255),
+		Email VARCHAR(255),
+		Sdt VARCHAR(255),
+		NgayLapTheDocGia SMALLDATETIME,
+		NgayHetHan SMALLDATETIME
+	)
 
-CREATE TABLE THUTHU(
-	MaTT VARCHAR(255) PRIMARY KEY,
-	HoTen VARCHAR(255),
-	MaTaiKhoan VARCHAR(255) REFERENCES TAIKHOAN(MaTK),
-	NgayTaoTK SMALLDATETIME,
-	NgaySinh SMALLDATETIME,
-	DiaChi VARCHAR(255),
-	Email VARCHAR(255),
-	Sdt VARCHAR(255)
-)
-
-
-
-CREATE TABLE THELOAI_SACH(
-	MaTheLoai VARCHAR(255) PRIMARY KEY,
-	TenTheLoai VARCHAR(255)
-)
-
-CREATE TABLE SACH(
-	MaSach VARCHAR(255) PRIMARY KEY,
-	TenSach VARCHAR(255),
-	MaTheLoai VARCHAR(255) REFERENCES THELOAI_SACH(MaTheLoai),
-	TacGia VARCHAR(255),
-	NamXuatBan INT,
-	NhaXuatBan VARCHAR(255),
-	SoLuong INT,
-	GiaSach MONEY
-)
-
-CREATE TABLE PHIEU_MUON_SACH(
-	MaPhieuMuon VARCHAR(255) PRIMARY KEY,
-	MaDG VARCHAR(255) REFERENCES THE_DOCGIA(MaDG),
-	MaTT VARCHAR(255) REFERENCES THUTHU(MaTT),
-	SoLuongSach INT,
-	NgayMuon SMALLDATETIME
-)
-
-CREATE TABLE PHIEU_TRA_SACH(
-	MaPhieuTra VARCHAR(255)  PRIMARY KEY,
-	MaPhieuMuon VARCHAR(255) REFERENCES PHIEU_MUON_SACH(MaPhieuMuon),
-	MaDG VARCHAR(255) REFERENCES THE_DOCGIA(MaDG),
-	MaTT VARCHAR(255) REFERENCES THUTHU(MaTT),
-	NgayTra SMALLDATETIME,
-	SoLuongSach INT,
-	SoNgayTre INT,
-	SoTienPhat MONEY
-)
-
-CREATE TABLE CHITIET_MUONTRA(
-	MaPhieuMuon VARCHAR(255) REFERENCES PHIEU_MUON_SACH(MaPhieuMuon),
-	MaSach VARCHAR(255) REFERENCES SACH(MaSach),
-	SoLuong INT,
-	CONSTRAINT PK_CTMT PRIMARY KEY (MaPhieuMuon, MaSach)
-)
-
-CREATE TABLE PHIEU_THU_TIEN_PHAT(
-	MaPhieuPhat VARCHAR(255) PRIMARY KEY,
-	MaDG VARCHAR(255) REFERENCES THE_DOCGIA(MaDG),
-	TongTienPhat MONEY,
-	TienNhanDuoc MONEY,
-	TienConLai MONEY,
-	NgayTraTien SMALLDATETIME
-)
+	CREATE TABLE THUTHU(
+		MaTT VARCHAR(255) PRIMARY KEY,
+		HoTen VARCHAR(255),
+		MaTaiKhoan VARCHAR(255) REFERENCES TAIKHOAN(MaTK),w
+		NgayTaoTK SMALLDATETIME,
+		NgaySinh SMALLDATETIME,
+		DiaChi VARCHAR(255),
+		Email VARCHAR(255),
+		Sdt VARCHAR(255)
+	)
+		
 
 
-CREATE TABLE QUY_DINH(
-	MinAge INT,
-	MaxAge INT,
-	Thoi_Han_The_Doc_Gia INT,
-	So_Nam_Xuat_Ban INT,
-	So_Sach_Doc_Gia_Muon_Duoc_Trong_Mot_ThoiGian INT,
-	So_Ngay_Gioi_Han_So_Luong_Sach_Muon INT,
-	Thoi_Han_Muon_Sach INT,
-	Phi_Phat_Theo_Mot_Ngay_Tre INT,
-)
+	CREATE TABLE THELOAI_SACH(
+		MaTheLoai VARCHAR(255) PRIMARY KEY,
+		TenTheLoai VARCHAR(255)
+	)
 
+	CREATE TABLE SACH(
+		MaSach VARCHAR(255) PRIMARY KEY,
+		TenSach VARCHAR(255),
+		MaTheLoai VARCHAR(255) REFERENCES THELOAI_SACH(MaTheLoai),
+		TacGia VARCHAR(255),
+		NamXuatBan INT,
+		NhaXuatBan VARCHAR(255),
+		SoLuong INT,
+		GiaSach MONEY
+	)
+
+	CREATE TABLE PHIEU_MUON_SACH(
+		MaPhieuMuon VARCHAR(255) PRIMARY KEY,
+		MaDG VARCHAR(255) REFERENCES THE_DOCGIA(MaDG),
+		MaTT VARCHAR(255) REFERENCES THUTHU(MaTT),
+		SoLuongSach INT,
+		NgayMuon SMALLDATETIME
+	)
+
+	CREATE TABLE PHIEU_TRA_SACH(
+		MaPhieuTra VARCHAR(255)  PRIMARY KEY,
+		MaPhieuMuon VARCHAR(255) REFERENCES PHIEU_MUON_SACH(MaPhieuMuon),
+		MaDG VARCHAR(255) REFERENCES THE_DOCGIA(MaDG),
+		MaTT VARCHAR(255) REFERENCES THUTHU(MaTT),
+		NgayTra SMALLDATETIME,
+		SoLuongSach INT,
+		SoNgayTre INT,
+		SoTienPhat MONEY
+	)
+
+	CREATE TABLE CHITIET_MUONTRA(
+		MaPhieuMuon VARCHAR(255) REFERENCES PHIEU_MUON_SACH(MaPhieuMuon),
+		MaSach VARCHAR(255) REFERENCES SACH(MaSach),
+		SoLuong INT,
+		CONSTRAINT PK_CTMT PRIMARY KEY (MaPhieuMuon, MaSach)
+	)
+
+	CREATE TABLE PHIEU_THU_TIEN_PHAT(
+		MaPhieuPhat VARCHAR(255) PRIMARY KEY,
+		MaDG VARCHAR(255) REFERENCES THE_DOCGIA(MaDG),
+		TongTienPhat MONEY,
+		TienNhanDuoc MONEY,
+		TienConLai MONEY,
+		NgayTraTien SMALLDATETIME
+	)
+
+
+	CREATE TABLE QUY_DINH(
+		MinAge INT,
+		MaxAge INT,
+		Thoi_Han_The_Doc_Gia INT,
+		So_Nam_Xuat_Ban INT,
+		So_Sach_Doc_Gia_Muon_Duoc_Trong_Mot_ThoiGian INT,
+		So_Ngay_Gioi_Han_So_Luong_Sach_Muon INT,
+		Thoi_Han_Muon_Sach INT,
+		Phi_Phat_Theo_Mot_Ngay_Tre INT,
+	)
+
+	CREATE TABLE ThoiGian_ThongKe(
+		Thang INT,
+		Nam INT,
+	)
+GO
 -------------------------------------------------------------
 
 --TAO TRIGGER
 
 --QD1: TUOI DOC GIA TU 18 DEN 55.
 
-CREATE TRIGGER KiemTraTuoi ON THE_DOCGIA
+CREATE OR ALTER TRIGGER KiemTraTuoi ON THE_DOCGIA
 FOR INSERT, UPDATE
 AS
 BEGIN
@@ -128,9 +134,9 @@ BEGIN
         ROLLBACK TRANSACTION
     END
 END
-
+GO
 --QD1: The doc gia co thoi han la vai thang theo quy dinh.
-CREATE TRIGGER HSD ON THE_DOCGIA
+CREATE OR ALTER TRIGGER HSD ON THE_DOCGIA
 AFTER INSERT, UPDATE
 AS
 BEGIN
@@ -140,10 +146,10 @@ BEGIN
     JOIN QUY_DINH Q ON 1=1
     WHERE td.MaDG = i.MaDG
 END
-
+GO
 --QD2: CHI NHAN SACH XUAT BAN TRONG VONG VAI NAM THEO QUY DINH.
 
-CREATE TRIGGER KiemTraNamXuatBan ON SACH
+CREATE OR ALTER TRIGGER KiemTraNamXuatBan ON SACH
 FOR INSERT, UPDATE
 AS
 BEGIN
@@ -169,20 +175,13 @@ BEGIN
         ROLLBACK TRANSACTION
     END
 END
-
+GO
 --QD3: Chi cho muon voi the con han. Chi muon duoc mot so luong sach nhat dinh trong mot thoi gian
 
 CREATE OR ALTER TRIGGER DieuKienMuon ON PHIEU_MUON_SACH
 FOR INSERT
 AS
 BEGIN
-    DECLARE @MaxBooksPerMember INT
-    DECLARE @MaxDaysToReturn INT
-
-    SELECT @MaxBooksPerMember = Q.So_Sach_Doc_Gia_Muon_Duoc_Trong_Mot_ThoiGian,
-           @MaxDaysToReturn = Q.So_Ngay_Gioi_Han_So_Luong_Sach_Muon
-    FROM QUY_DINH Q
-
     -- Kiểm tra các bản ghi trong inserted
     IF EXISTS (
         SELECT 1
@@ -194,26 +193,72 @@ BEGIN
         PRINT 'The doc gia da het han'
         ROLLBACK TRANSACTION
     END
-
-    -- Kiểm tra số lượng sách mượn của từng độc giả trong khoảng thời gian quy định
-    IF EXISTS (
-        SELECT 1
-        FROM (
-            SELECT MaDG, SUM(SoLuongSach) AS TongSoLuongSachMuon
-            FROM PHIEU_MUON_SACH
-            WHERE MaDG IN (SELECT MaDG FROM inserted)
-              AND NgayMuon >= DATEADD(DAY, -@MaxDaysToReturn, GETDATE())
-            GROUP BY MaDG
-        ) AS A
-        WHERE A.TongSoLuongSachMuon > @MaxBooksPerMember
-    )
-    BEGIN
-        PRINT 'So luong sach muon da vuot muc toi da.'
-        ROLLBACK TRANSACTION
-    END
 END
+GO
+
+-- Kiem tra dieu kien "Chi muon duoc mot so luong sach nhat dinh trong mot thoi gian"
+CREATE OR ALTER TRIGGER trg_KiemTraSoLuongSachMuon
+ON PHIEU_MUON_SACH
+FOR INSERT
+AS
+BEGIN
+    -- Tạo bảng tạm thời để lưu thông tin về độc giả và số lượng sách mượn trong khoảng thời gian 7 ngày
+    CREATE TABLE #TempMuonSach (
+        MaDG VARCHAR(255),
+        NgayMuon SMALLDATETIME,
+        SoLuongSach INT
+    );
+
+
+    INSERT INTO #TempMuonSach (MaDG, NgayMuon, SoLuongSach)
+    SELECT MaDG, NgayMuon, SoLuongSach
+    FROM INSERTED;
+
+
+    DECLARE @MaDG VARCHAR(255);
+    DECLARE @TongSoLuongSach INT;
+	DECLARE @MaxBooksPerMember INT
+    DECLARE @MaxDaysToReturn INT
+
+    SELECT @MaxBooksPerMember = Q.So_Sach_Doc_Gia_Muon_Duoc_Trong_Mot_ThoiGian,
+           @MaxDaysToReturn = Q.So_Ngay_Gioi_Han_So_Luong_Sach_Muon
+    FROM QUY_DINH Q
+
+
+    DECLARE cur CURSOR FOR
+    SELECT MaDG, SUM(SoLuongSach)
+    FROM #TempMuonSach
+    GROUP BY MaDG;
+
+    OPEN cur;
+
+    FETCH NEXT FROM cur INTO @MaDG, @TongSoLuongSach;
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        SELECT @TongSoLuongSach = @TongSoLuongSach + ISNULL(SUM(SoLuongSach), 0)
+        FROM PHIEU_MUON_SACH
+        WHERE MaDG = @MaDG AND NgayMuon >= DATEADD(DAY, @MaxDaysToReturn, GETDATE());
+
+
+        IF (@TongSoLuongSach > @MaxBooksPerMember)
+        BEGIN
+            PRINT N'Độc giả ' + @MaDG + N' chỉ được mượn tối đa ' + CONVERT(VARCHAR(255), @MaxBooksPerMember) + N' quyển sách trong ' + CONVERT(VARCHAR(255), @MaxDaysToReturn) + N' ngày.';
+            ROLLBACK TRANSACTION;
+        END
+
+        FETCH NEXT FROM cur INTO @MaDG, @TongSoLuongSach;
+    END
+
+    CLOSE cur;
+    DEALLOCATE cur;
+
+    DROP TABLE #TempMuonSach;
+END;
+GO
+
 -- QD5: Moi ngay tra tre phat 1.000 dong/ngay. (Moi sach duoc muon toi da 10 ngay.)
-CREATE TRIGGER TinhPhiPhat ON PHIEU_TRA_SACH
+CREATE OR ALTER TRIGGER TinhPhiPhat ON PHIEU_TRA_SACH
 AFTER INSERT, UPDATE
 AS
 BEGIN
@@ -237,9 +282,9 @@ BEGIN
     JOIN PHIEU_MUON_SACH pm ON pts.MaPhieuMuon = pm.MaPhieuMuon
     JOIN inserted i ON pts.MaPhieuTra = i.MaPhieuTra
 END
-
+GO
 --QD: So tien thu khong duoc vuot qua so tien doc gia dang no.
-CREATE TRIGGER ThuTienPhat ON PHIEU_THU_TIEN_PHAT
+CREATE OR ALTER TRIGGER ThuTienPhat ON PHIEU_THU_TIEN_PHAT
 AFTER INSERT, UPDATE
 AS
 BEGIN
@@ -251,3 +296,5 @@ BEGIN
     FROM PHIEU_THU_TIEN_PHAT p
     JOIN inserted i ON p.MaPhieuPhat = i.MaPhieuPhat;
 END
+GO
+
